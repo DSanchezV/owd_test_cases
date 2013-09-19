fb06d6b858e11145c2ea03b738ba2f519a7cf7ba
roy <roy.collings@sogeti.com>
2013-09-19 17:50:49 +0200

    Added 26318

---
+++ tests/HOMESCREEN/test_26318.py
@@ -0,0 +1,39 @@
+#
+# Imports which are standard for all test cases.
+#
+import sys
+sys.path.insert(1, "./")
+from gaiatest   import GaiaTestCase
+from OWDTestToolkit import *
+
+#
+# Imports particular to this test case.
+#
+
+class test_main(GaiaTestCase):
+    
+    def setUp(self):
+        #
+        # Set up child objects...
+        #
+        GaiaTestCase.setUp(self)
+        
+        self.UTILS      = UTILS(self)
+        self.Settings   = Settings(self)
+        self.EME        = EverythingMe(self)
+        self.actions    = Actions(self.marionette)
+        
+    def tearDown(self):
+        self.UTILS.reportResults()
+        
+    def test_run(self):    
+        self.lockscreen.lock()
+        x = self.UTILS.screenShotOnErr()
+        self.UTILS.logResult("info", "Locked screen:", x)
+        self.UTILS.waitForElements( ("id", "lockscreen-area-handle"), "Lockscreen area", True, 1, False)
+
+        self.lockscreen.unlock()
+        x = self.UTILS.screenShotOnErr()
+        self.UTILS.logResult("info", "unlocked screen:", x)
+        
+        self.UTILS.waitForNotElements( ("id", "lockscreen-area-handle"), "Lockscreen area", True, 1, False)
