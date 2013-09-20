3fea5456119573bde680db373985d1b5f5b21c15
roy <roy.collings@sogeti.com>
2013-09-20 14:57:37 +0200

    Added 26586

--- tests/BROWSER/test_26586.py
+++ tests/BROWSER/test_26586.py
@@ -35,13 +35,27 @@ class test_main(GaiaTestCase):
         # Open the browser app. and search for 'something'.
         #
         self.Browser.launch()
+        self.Browser.searchUsingUrlField("one")
         
         #
         # Now open the tabs list and check our tab is there.
         #
-        self.UTILS.TEST(self.Browser.trayCounterIs(2), "Tab tray counter is '1'.", False)
+        self.UTILS.TEST(self.Browser.trayCounterIs(1), "Tab tray counter is '1'.", False)
         
-        x = self.UTILS.getElement(DOM.Browser.tab_tray_open, "Tab tray open button")
-        x.tap()
+        self.Browser.addNewTab()
+                
+        self.Browser.searchUsingUrlField("two")
+        self.UTILS.TEST(self.Browser.trayCounterIs(2), "Tab tray counter is '2'.", False)
 
+        self.Browser.addNewTab()
+        
+        self.Browser.searchUsingUrlField("three")
+        self.UTILS.TEST(self.Browser.trayCounterIs(3), "Tab tray counter is '3'.", False)
+
+        self.Browser.addNewTab()
+        
+        self.Browser.searchUsingUrlField("four")
+        self.UTILS.TEST(self.Browser.trayCounterIs(4), "Tab tray counter is '4'.", False)
+
+        
         
